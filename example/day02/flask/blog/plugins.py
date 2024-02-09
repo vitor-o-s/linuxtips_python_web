d17bf25589baa127bd2c1f3872017938f78efe22
Vitor O <vitoros@outlook.com.br>
2024-02-09 15:50:55 -0300

    Filtros de formatação no template

---
+++ example/day02/flask/blog/plugins.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from flask import Flask
+from mistune import markdown
+
+
+def configure(app: Flask):
+    app.add_template_global(markdown)
+    app.add_template_filter(lambda date: date.strftime('%d/%m/%Y'), 'format_date')
