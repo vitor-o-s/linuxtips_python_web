84069637b8460475b82cf2fb8c95f862c33172b2
Vitor O <vitoros@outlook.com.br>
2024-02-09 15:51:23 -0300

    Ordenação dos posts

--- example/day02/flask/blog/posts.py
+++ example/day02/flask/blog/posts.py
@@ -1,10 +1,12 @@
+import pymongo
+
 from blog.database import mongo
 from datetime import datetime
 
 
 def  get_all_posts(published: bool = True):
     posts = mongo.db.posts.find({'published': published})
-    return posts.sort('date')
+    return posts.sort('date', pymongo.DESCENDING)
 
 def get_post_by_slug(slug: str) -> dict:
     post = mongo.db.posts.find_one({'slug': slug})
