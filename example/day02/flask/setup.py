6e06f65c2a81491a94106629e213fac045f6d545
Vitor O <vitoros@outlook.com.br>
2024-02-14 09:08:02 -0300

    Interface administratitva

--- example/day02/flask/setup.py
+++ example/day02/flask/setup.py
@@ -11,7 +11,6 @@ setup(
         "flask-bootstrap",
         "mistune",
         "flask-simplelogin",
-        # "flask-admin",
-        # "flask-admin @ git+ssh://git@github.com/flask-admin/flask-admin#egg=flask-admin"
+        "flask-admin"
     ],
 )
