a7c9fcc23e34defaa5bda25b7705825752364234
Vitor O <vitoros@outlook.com.br>
2024-02-14 19:18:07 -0300

    Model base

--- example/day02/django/blog/models.py
+++ example/day02/django/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-date"]
