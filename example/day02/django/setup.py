218e494bc61a0e75e702716383a63343ae214b7d
Vitor O <vitoros@outlook.com.br>
2024-02-14 09:55:45 -0300

    Tornando o projeto instalavel para facilitar a chamada do django-admin

---
+++ example/day02/django/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="django_blog",
+    version="0.1.0",
+    packages=["djblog", "blog"],
+    install_requires=[
+        "django" 
+        # "django-markdownify", 
+        # "django-extensions"
+    ],
+)
