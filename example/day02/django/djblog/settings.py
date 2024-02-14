8798c59cee629a0db89793a06fc4c0011f50b3d1
Vitor O <vitoros@outlook.com.br>
2024-02-14 19:17:40 -0300

    Extensão para melhorar o uso do django-admin

--- example/day02/django/djblog/settings.py
+++ example/day02/django/djblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'blog', # Necessário para que o django reconheça o app 
 ]
 
