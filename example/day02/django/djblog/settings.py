69d128c6671e7f79ebaa31af17181e2ff86f3537
Vitor O <vitoros@outlook.com.br>
2024-02-14 19:44:20 -0300

    Adicionando markdown as postagens

--- example/day02/django/djblog/settings.py
+++ example/day02/django/djblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'markdownify.apps.MarkdownifyConfig',
     'blog', # Necessário para que o django reconheça o app 
 ]
 
